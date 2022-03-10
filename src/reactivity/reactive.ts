268e7640be604d5ac76325ac130952b8f2b4a179
WUMANHO <wumanho@tutanota.com>
2022-03-10 22:27:33 +0800

    commit readonly 初步实现

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,14 +1,21 @@
-import {track,trigger} from "./effect";
+import {track, trigger} from "./effect";
+
+function createGetter(isReadonly: boolean) {
+    return function (target, key) {
+        const res = Reflect.get(target, key)
+        if (!isReadonly) {
+            //依赖收集
+            track(target, key)
+        }
+        return res
+    }
+}
+
 
 export function reactive(raw) {
     return new Proxy(raw, {
         //target = 原始对象raw，key就是用户访问的那个key
-        get(target, key) {
-            const res = Reflect.get(target, key)
-            //依赖收集
-            track(target, key)
-            return res
-        },
+        get: createGetter(false),
         //set的时候需要触发收集的依赖
         set(target, key, value) {
             const res = Reflect.set(target, key, value)
@@ -16,6 +23,15 @@ export function reactive(raw) {
             trigger(target, key)
             //最后当然是要做set该做的事
             return res
+        }
+    })
+}
+
+export function readonly(raw) {
+    return new Proxy(raw, {
+        get: createGetter(true),
+        set(target, key, value) {
+            return true
         }
     })
 }
