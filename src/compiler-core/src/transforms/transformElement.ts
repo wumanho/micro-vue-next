2261897401bae074dd9352bc402351d837550294
WUMANHO <wumanho@tutanota.com>
2022-05-15 13:32:49 +0800

    commit 组装三种类型-2

--- src/compiler-core/src/transforms/transformElement.ts
+++ src/compiler-core/src/transforms/transformElement.ts
@@ -1,13 +1,11 @@
-import {NodeTypes} from "../ast";
+import {createVNodeCall, NodeTypes} from "../ast";
 import {CREATE_ELEMENT_VNODE} from "../runtimeHelpers";
 
 export function transformElement(node, context) {
     if (node.type === NodeTypes.ELEMENT) {
         return () => {
-            context.helper(CREATE_ELEMENT_VNODE)
-
             //tag
-            const vnodeTag = node.tag
+            const vnodeTag = `"${node.tag}"`
             //props
             let vnodeProps
 
@@ -15,15 +13,7 @@ export function transformElement(node, context) {
             const children = node.children
             let vnodeChildren = children[0]
             const child = children[0]
-
-            const vnodeElement = {
-                type: NodeTypes.ELEMENT,
-                tag: vnodeTag,
-                props: vnodeProps,
-                children: vnodeChildren
-            }
-
-            node.codegenNode = vnodeElement
+            node.codegenNode = createVNodeCall(context, vnodeTag, vnodeProps, vnodeChildren)
         }
     }
 }
